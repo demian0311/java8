898ff5c3749a8c7b52e3b906f1b1cd98c8a77710
Demian Neidetcher <demian0311@gmail.com>
2014-06-02 09:19:13 -0500

    streams

--- src/test/java/com/neidetcher/java8/_02_streams/Streams.java
+++ src/test/java/com/neidetcher/java8/_02_streams/Streams.java
@@ -3,11 +3,13 @@ package com.neidetcher.java8._02_streams;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Streams {
 
@@ -45,4 +47,77 @@ public class Streams {
 
         assertEquals(Arrays.asList(4, 5), sortedMeatListStringSize);
     }
+
+    @Test public void source(){
+        Stream<String> groceryListStream = groceryList.stream();
+        assertEquals(5, groceryListStream.count());
+    }
+
+    @Test public void filter(){
+        List<String> bGroceryList = groceryList
+                .stream()                             // source
+                .filter(curr -> curr.startsWith("b")) // intermediate
+                .collect(Collectors.toList());        // terminal
+
+        assertEquals(Arrays.asList("bacon", "brussels sprouts"), bGroceryList);
+    }
+
+    @Test public void map(){
+        List<Integer> bGroceryList = groceryList
+                .stream()                             // source
+                .filter(curr -> curr.startsWith("b")) // intermediate
+                .map(String::length)                  // intermediate
+                .collect(Collectors.toList());        // terminal
+
+        assertEquals(Arrays.asList(5, 16), bGroceryList);
+    }
+
+    @Test public void sorted(){
+        List<String> bGroceryList = groceryList
+                .stream()                             // source
+                .filter(curr -> curr.startsWith("b")) // intermediate
+                .sorted()                             // intermediate
+                .collect(Collectors.toList());        // terminal
+
+        assertEquals(Arrays.asList("bacon", "brussels sprouts"), bGroceryList);
+    }
+
+    @Test public void forEach(){
+        IntStream
+                .range(0, 10)                  // source
+                .forEach(System.out::println); // terminal
+    }
+
+    @Test public void reduce(){
+        int evenNumbersSum = IntStream
+                .range(0, 10)                  // source
+                .filter(curr -> curr % 2 == 0) // intermediate
+                .reduce(0, Integer::sum);      // terminal
+
+        assertEquals(20, evenNumbersSum);
+    }
+
+    @Test public void collect(){
+        List<String> bGroceryList = groceryList
+                .stream()                             // source
+                .filter(curr -> curr.startsWith("b")) // intermediate
+                .sorted()                             // intermediate
+                .collect(Collectors.toList());        // terminal
+
+        assertEquals(Arrays.asList("bacon", "brussels sprouts"), bGroceryList);
+    }
+
+    @Test public void collectMultipleArgument(){
+        List<String> bGroceryList = groceryList
+                .stream()                             // source
+                .filter(curr -> curr.startsWith("b")) // intermediate
+                .sorted()                             // intermediate
+                .collect(                             // terminal
+                        ArrayList::new,
+                        ArrayList::add,
+                        ArrayList::addAll);
+
+        assertEquals(Arrays.asList("bacon", "brussels sprouts"), bGroceryList);
+    }
 }
+
